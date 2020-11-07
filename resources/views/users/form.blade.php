d0b26a6901de9381106dbb974b5ec1e417a8105e
Diogo Lindoso <diogolindoso@gmail.com>
2020-11-07 10:23:26 -0400

    Criando view com template do AdminLTE

---
+++ resources/views/users/form.blade.php
@@ -0,0 +1,17 @@
+@extends('adminlte::page')
+
+@section('title', 'Cadastrado de Usuário.')
+
+@section('content_header')
+    <h1>Cadastrado de Usuário.</h1>
+@stop
+
+@section('content')
+    <p>Aqui vai o formulário de cadastro!</p>
+@stop
+
+@section('css')
+@stop
+
+@section('js')
+@stop
