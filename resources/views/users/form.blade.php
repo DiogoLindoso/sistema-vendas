a25f8bce88f3100d947624a8b7466cff85a376ed
Diogo Lindoso <diogolindoso@gmail.com>
2020-11-07 11:52:39 -0400

    Alterado formulário para inserir Usuario

--- resources/views/users/form.blade.php
+++ resources/views/users/form.blade.php
@@ -7,7 +7,44 @@
 @stop
 
 @section('content')
-    <p>Aqui vai o formulário de cadastro!</p>
+    <div class="card card-primary">
+        <!-- form start -->
+        {!! Form::open(['route' => 'users.store']) !!}
+        <div class="card-body">
+            <div class="form-group">
+                {!! Form::label('name', 'Nome') !!}
+                {!! Form::text('name', null, ['class' => 'form-control']) !!}
+                @error('name')
+                    <div class="text-danger">{{$message}}</div>
+                @enderror
+            </div>
+            <div class="form-group">
+                {!! Form::label('email', 'Email') !!}
+                {!! Form::email('email', null, ['class' => 'form-control']) !!}
+                @error('email')
+                   <div class="text-danger">{{$message}}</div>
+                @enderror
+            </div>
+            <div class="form-group">
+                {!! Form::label('password', 'Senha') !!}
+                {!! Form::password('password',['class' => 'form-control']) !!}
+                @error('password')
+                   <div class="text-danger">{{$message}}</div>
+                @enderror
+            </div>
+            <div class="form-group">
+                {!! Form::label('type', 'Tipo de Usuário') !!}
+                {!! Form::select('type', ['0'=>'Vendedor', '1'=>'Administrador'], null, ['class' => 'form-control']) !!}
+                @error('type')
+                    <div class="text-danger">{{$message}}</div>
+                @enderror
+            </div>
+          <!-- /.card-body -->
+            <div class="card-footer">
+                {!! Form::submit('Salvar',['class' => 'btn btn-primary']) !!}
+            </div>
+        {!! Form::close() !!}
+    </div>
 @stop
 
 @section('css')
