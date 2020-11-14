39b1b7d4f9e4c57c64d5b397aa6560934da24e5a
Diogo Lindoso <diogolindoso@gmail.com>
2020-11-14 11:14:37 -0400

    Configurando e ativando datatable
    https://yajrabox.com/docs/laravel-datatables/master/installation

--- config/adminlte.php
+++ config/adminlte.php
@@ -300,7 +300,7 @@ return [
 
     'plugins' => [
         'Datatables' => [
-            'active' => false,
+            'active' => true,
             'files' => [
                 [
                     'type' => 'js',
